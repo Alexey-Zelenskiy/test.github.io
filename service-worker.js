e26585420d095c5f1d4ec9deab38885c48225430
Alexey Zelenskiy <zelenskii1999@mail.ru>
2019-12-16 15:03:19 +0700

    build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Alexey-Zelenskiy/Alexey-Zelenskiy.github.io/precache-manifest.ccd780765571e31ec5b2dd4af709fcc7.js"
+  "/test.github.io/precache-manifest.553aac114c0fc90b9af39f69cd71e0e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Alexey-Zelenskiy/Alexey-Zelenskiy.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
