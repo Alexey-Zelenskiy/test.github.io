c52f806c750ca8c60d47eb5a93954713dc9d1f74
Alexey Zelenskiy <zelenskii1999@mail.ru>
2019-12-16 15:24:53 +0700

    build

---
+++ precache-manifest.4326328304a5c4561aa7d10fa3a0ca33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c527fd050ed3e2e6833562b39e293aa3",
+    "url": "/test.github.io/index.html"
+  },
+  {
+    "revision": "53658df0d8e22def6246",
+    "url": "/test.github.io/static/css/main.22a8b602.chunk.css"
+  },
+  {
+    "revision": "032eb89f04c225bf897a",
+    "url": "/test.github.io/static/js/2.2c1aba3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test.github.io/static/js/2.2c1aba3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53658df0d8e22def6246",
+    "url": "/test.github.io/static/js/main.97a8ec54.chunk.js"
+  },
+  {
+    "revision": "56282cc673963293bd49",
+    "url": "/test.github.io/static/js/runtime-main.c87176b2.js"
+  },
+  {
+    "revision": "12e40dd1da9e83a03ca7e3d6a2679b09",
+    "url": "/test.github.io/static/media/error.12e40dd1.jpeg"
+  }
+]);
